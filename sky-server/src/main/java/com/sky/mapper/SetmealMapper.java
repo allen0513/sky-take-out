af875d503ee864a2605b194f2cfa829356bf182a
allen0513 <3043730186@qq.com>
2026-07-25 08:19:23 +0800

    缓存菜品代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -54,7 +54,7 @@ public interface SetmealMapper {
     void deleteById(Long setmealId);
 
     /**
-     * 修改套餐
+     * 根据id修改套餐
      * @param setmeal
      */
     @AutoFill(OperationType.UPDATE)
@@ -76,4 +76,5 @@ public interface SetmealMapper {
             "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
             "where sd.setmeal_id = #{setmealId}")
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
 }
